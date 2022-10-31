1bb7bf97f87350f53245b0f900f7d79b9701a41e
Adi Pratama Putra <pratama.77.sap@gmail.com>
2022-10-31 21:11:59 +0700

    CRUD Kasir

--- app.js
+++ app.js
@@ -15,108 +15,45 @@ var conn = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
-    database: "paw2"
+    database: "pos"
 });
 
-// app.get('/',(req,res)=>{
-//     pool.query('select * from app.buku', (error,results)=>{
-//         if(error){
-//             throw error;
-//         }
-//         res.render('index.ejs', {daftarBuku:results.rows});
-//     })
-// });
-
-
-app.get('/',(req, res) => {
-    let sql = "SELECT * FROM kasir";
-    let query = conn.query(sql, (err, results) => {
-      if(err) throw err;
-      res.render('manager_kasir.ejs',{
-        results: results
-      });
+app.get('/kasir',(req,res)=>{
+  let sql = "SELECT * FROM kasir";
+  let query = conn.query(sql, (err, results) => {
+    if(err) throw err;
+    res.render('manager_kasir.ejs',{
+      listKasir: results
     });
   });
-
-  app.get('/tambah-kasir',(req,res)=>{
-    res.render('manager_kasir_tambah.ejs');
 });
 
-app.post('/tambah', (req,res)=>{
-
-    let data = {
-        nama_kasir: req.body.nama_kasir, 
-        alamat: req.body.alamat,
-        no_telepon : req.body.no_telepon,
-        jenis_kelamin : req.body.jenis_kelamin,
-        tanggal_bekerja : req.body.tanggal_bekerja,
-        kode_kasir : req.body.kode_kasir
-    };
+app.post('/add-kasir',(req, res) => {
+  let data = {
+    nama_kasir: req.body.nama_kasir, 
+    no_hp: req.body.no_hp,
+    alamat: req.body.alamat_kasir,
+    kode_kasir: req.body.kode_kasir
+  };
   let sql = "INSERT INTO kasir SET ?";
   let query = conn.query(sql, data,(err, results) => {
     if(err) throw err;
-    res.redirect('/');
+    res.redirect('/kasir');
   });
-    
 });
 
-app.get('/tes',(req, res) => {
-  let sql = "SELECT * FROM tes";
+app.post('/update-kasir',(req, res) => {
+  let sql = "UPDATE kasir SET nama_kasir='"+req.body.nama_kasir+"', no_hp='"+req.body.no_hp+"', alamat='"+req.body.alamat_kasir+"', kode_kasir='"+req.body.kode_kasir+"' WHERE id="+req.body.id;
   let query = conn.query(sql, (err, results) => {
     if(err) throw err;
-    res.render('tes_index.ejs',{
-      results: results
-    });
+    res.redirect('/kasir');
   });
 });
 
-app.post('/save',(req, res) => {
-  // var data = {
-  //   nama: "adi", 
-  //   angka: "23"
-  // };
-  // var nama = req.body.nama;
-  // var angka = req.body.angka;
-
-  // var sql = "INSERT INTO tes (nama,angka) values  ('"+req.body.nama+"','"+req.body.angka+"')";
-  // var query = conn.query(sql, function (err, results) {
-  //   if(err) throw err;
-  //   res.redirect('/tes');
-  // });
-
-  let data = {
-    nama: req.body.nama, 
-    angka: req.body.angka
-  };
-  let sql = "INSERT INTO tes SET ?";
-  let query = conn.query(sql, data,(err, results) => {
-    if(err) throw err;
-    res.redirect('/tes');
-  });
-});
- 
-//route for update data
-app.post('/update',(req, res) => {
-  let sql = "UPDATE tes SET nama='"+req.body.nama+"', angka='"+req.body.angka+"' WHERE id="+req.body.id;
+app.post('/delete-kasir',(req, res) => {
+  let sql = "DELETE FROM kasir WHERE id="+req.body.id+"";
   let query = conn.query(sql, (err, results) => {
     if(err) throw err;
-    res.redirect('/tes');
+      res.redirect('/kasir');
   });
 });
- 
-//route for delete data
-app.post('/delete',(req, res) => {
-  let sql = "DELETE FROM tes WHERE id="+req.body.product_id+"";
-  let query = conn.query(sql, (err, results) => {
-    if(err) throw err;
-      res.redirect('/tes');
-  });
-});
-
-// con.connect(function(err) {
-//     if (err) throw err;
-//         con.query("SELECT * FROM kasir", function (err, result, fields) {
-//       if (err) throw err;
-//       console.log(result);
-//     });
-// });
