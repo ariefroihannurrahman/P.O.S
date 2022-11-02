53955ba8e9e911a8e5eb768dac1b5ed6816c46e9
AppAdi <pratama.77.sap@gmail.com>
2022-11-02 10:31:41 +0700

    Link UI Login

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.listen(3000, () => {
 });
 
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 var conn = mysql.createConnection({
     host: "localhost",
@@ -21,32 +23,76 @@ var conn = mysql.createConnection({
 });
 
 
-//login
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true,
+  loggedin: false
+}));
 
-app.get('/', (req,res) => {
-  res.render('login.ejs');
+// let date_time = new Date();
+// let date1 = ("0" + date_time.getDate()).slice(-2);
+// let month1 = ("0" + (date_time.getMonth() + 1)).slice(-2);
+// let year1 = date_time.getFullYear();
+// let tanggal = year1 + "-" + month1 + "-" + date1;
+
+
+app.get('/',(req,res)=>{
+  if(req.session.loggedin == true){
+    res.redirect('/kasir');
+  }else{
+    res.render('login.ejs');
+  }
 });
 
-app.get('/cari-produk', (req,res) => {
-  res.render('kasir/cari-produk.ejs');
+app.post('/auth', function(req,res) {
+  let kode = req.body.kode_kasir;
+  conn.query('SELECT * FROM karyawan WHERE kode = ?', [kode], function(error, results, fields) {
+    if (error) throw error;
+      if (results.length > 0) {
+          req.session.loggedin = true;
+          req.session.dataKasir = results[0];
+          res.redirect('/laporan-awal');
+      } else {
+        res.send('Kode Salah!');
+      }			
+      res.end();
+    });
 });
 
-app.get('/', (req,res) => {
-  res.render('login.ejs');
-});
-
-
-app.get('/manager', (req,res) => {
-  res.render('manager/home-manager.ejs');
-});
+//LAPORAN AWAL
 
 app.get('/laporan-awal', (req,res) => {
-  res.render('kasir/laporan-awal.ejs');
+  res.render('kasir/laporan-awal.ejs',{
+    dataKasir: req.session.dataKasir,
+  });
+});
+
+app.post('/add-laporan-awal',(req, res) => {
+  let data = {
+    no_karyawan : req.session.dataKasir.no_karyawan,
+    tanggal_laporan : tanggal,
+    laporan_awal : req.body.laporan_awal
+  };
+  let sql = "INSERT INTO laporan_kasir SET ?";
+  let query = conn.query(sql, data,(err, results) => {
+    if(err) throw err;
+    res.redirect('/kasir');
+  });
 });
 
 app.get('/laporan-akhir', (req,res) => {
   res.render('kasir/laporan-akhir.ejs');
 });
+
+
+//============
+
+app.get('/cari-produk', (req,res) => {
+  res.render('kasir/cari-produk.ejs');
+});
+
+
 
 app.get('/kasir', (req,res) => {
   res.render('kasir/home-kasir.ejs');
@@ -60,25 +106,9 @@ app.get('/owner', (req,res) => {
   res.render('owner/laporan-pemilik.ejs');
 });
 
-app.get('/penjualan', (req,res) => {
-  res.render('manager/kelola-penjualan.ejs');
-});
 
-app.get('/produk', (req,res) => {
-  res.render('manager/kelola-produk.ejs');
-});
 
-app.get('/karyawan', (req,res) => {
-  res.render('manager/kelola-karyawan.ejs');
-});
 
-app.get('/kategori', (req,res) => {
-  res.render('manager/kelola-kategori.ejs');
-});
-
-app.get('/tambah-jenis', (req,res) => {
-  res.render('manager/tambah/tambah-jenis.ejs');
-});
 
 app.get('/edit-jenis', (req,res) => {
   res.render('manager/edit/edit-jenis.ejs');
@@ -86,10 +116,6 @@ app.get('/edit-jenis', (req,res) => {
 
 app.get('/edit-kategori', (req,res) => {
   res.render('manager/edit/edit-kategori.ejs');
-});
-
-app.get('/edit-karyawan', (req,res) => {
-  res.render('manager/edit/edit-karyawan.ejs');
 });
 
 app.get('/edit-produk', (req,res) => {
@@ -104,62 +130,14 @@ app.get('/edit-kategori', (req,res) => {
   res.render('manager/edit/edit-kategori.ejs');
 });
 
-app.get('/tambah-produk', (req,res) => {
-  res.render('manager/tambah/tambah-produk.ejs');
-});
-
-app.get('/tambah-karyawan', (req,res) => {
-  res.render('manager/tambah/tambah-karyawan.ejs');
-});
-
 app.get('/tambah-penjualan', (req,res) => {
   res.render('manager/tambah/tambah-penjualan.ejs');
 });
 
-app.get('/jenis', (req,res) => {
-  res.render('manager/kelola-jenis.ejs');
-});
+
+
 
 // //HALAMAN LOGIN KASIR
-
-// app.use(session({
-// 	secret: 'secret',
-// 	resave: true,
-// 	saveUninitialized: true,
-//   loggedin: false
-// }));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
-// app.get('/',(req,res)=>{
-//   if(req.session.loggedin){
-//     res.redirect('/transaksi');
-//   }else{
-//     res.render('transaksi/login.ejs');
-//   }
-    
-// });
-
-
-// app.post('/auth', function(req,res) {
-
-// 	let kode = req.body.kode_kasir;
-
-// 	conn.query('SELECT * FROM kasir WHERE kode_kasir = ?', [kode], function(error, results, fields) {
-// 		if (error) throw error;
-//       if (results.length > 0) {
-//           req.session.loggedin = true;
-//           req.session.dataKasir = results[0];
-//           // req.session.nama_kasir = results[0].nama_kasir;
-//           res.redirect('/transaksi');
-//       } else {
-// 				res.send('Kode Salah!');
-// 			}			
-// 			res.end();
-// 		});
-// });
-
-
 // app.get('/transaksi', function(req,res) {
 // 	if (req.session.loggedin) {
 //     res.render('transaksi/transaksi.ejs',{
@@ -177,25 +155,147 @@ app.get('/jenis', (req,res) => {
 //   res.redirect('/');
 // });
 
+//HALAMAN HOME MANAGER
 
-// //HALAMAN KELOLA KASIR MANAGER
+app.get('/manager/',(req,res)=>{
+  async.parallel([
+    function(callback){
+      let sql = "select jadwal.id, jadwal.hari, shift.nama_shift, shift.waktu_mulai, shift.waktu_selesai, kasir.nama_kasir FROM jadwal INNER JOIN shift on jadwal.id_shift = shift.id INNER JOIN kasir ON jadwal.id_kasir = kasir.id;";
+      let query = conn.query(sql, (err, results1) => {
+        if (err) {
+          return callback(err);
+        }
+        return callback(null, results1);
+      });
+    },function(callback){
+      let sql = "SELECT * FROM shift";
+      let query = conn.query(sql, (err, results2) => {
+        if (err) {
+          return callback(err);
+        }
+        return callback(null, results2);
+      });
+    },function(callback){
+      let sql = "SELECT * FROM kasir";
+      let query = conn.query(sql, (err, results3) => {
+        if (err) {
+          return callback(err);
+        }
+        return callback(null, results3);
+      });
+    }
+  ], function(error,callbackResults){
+    if(error){
+      console.log(error);
+    }else{
+      res.render('manager/home-manager.ejs',{
+        listJadwal:callbackResults[0],
+        listShift:callbackResults[1],
+        listKasir: callbackResults[2]
+      });
+    }
+  });
 
-// app.get('/manager/kasir',(req,res)=>{
-//   let sql = "SELECT * FROM kasir";
-//   let query = conn.query(sql, (err, results) => {
-//     if(err) throw err;
-//     res.render('manager/manager_kasir.ejs',{
-//       listKasir: results
-//     });
-//   });
-// });
+// HALAMAN KELOLA  MANAGER
 
-// app.post('/add-kasir',(req, res) => {
+app.get('/karyawan', (req,res) => {
+  let sql = "SELECT * FROM karyawan";
+  let query = conn.query(sql, (err, results) => {
+    if(err) throw err;
+    res.render('manager/kelola-karyawan.ejs',{
+      listKasir: results,
+    });
+  });
+});
+
+app.get('/tambah-karyawan', (req,res) => {
+  res.render('manager/tambah/tambah-karyawan.ejs');
+});
+
+
+// HALAMAN KELOLA BARANG
+app.get('/produk', (req,res) => {
+  let sql = "SELECT produk.no_produk, produk.kd_produk, produk.nama_produk, produk.harga, jenis.nama_jenis, kategori.nama_kategori FROM produk INNER JOIN jenis ON produk.no_jenis = jenis.no_jenis INNER JOIN kategori ON produk.no_kategori = kategori.no_kategori";
+  let query = conn.query(sql, (err, results) => {
+    if(err) throw err;
+    res.render('manager/kelola-produk.ejs',{
+      listProduk: results,
+    });
+  });
+});
+
+app.get('/tambah-produk', (req,res) => {
+  res.render('manager/tambah/tambah-produk.ejs');
+});
+
+
+//HALAMAN KELOLA PENJUALAN
+
+app.get('/penjualan', (req,res) => {
+  let sql = "select penjualan.no_penjualan, penjualan.kd_penjualan, karyawan.nama_karyawan, penjualan.tanggal_penjualan, penjualan.deskripsi from penjualan INNER JOIN karyawan ON penjualan.no_karyawan = karyawan.no_karyawan;";
+  let query = conn.query(sql, (err, results) => {
+    if(err) throw err;
+    res.render('manager/kelola-penjualan.ejs',{
+      listPenjualan: results,
+    });
+  });
+});
+
+//HALAMAN KELOLA KATEGORI
+app.get('/kategori', (req,res) => {
+  let sql = "SELECT * FROM kategori";
+  let query = conn.query(sql, (err, results) => {
+    if(err) throw err;
+    res.render('manager/kelola-kategori.ejs',{
+      listKategori: results,
+    });
+  });
+});
+
+
+//HALAMAN KELOLA JENIS
+app.get('/jenis', (req,res) => {
+  let sql = "SELECT * FROM jenis";
+  let query = conn.query(sql, (err, results) => {
+    if(err) throw err;
+    res.render('manager/kelola-jenis.ejs',{
+      listJenis: results,
+    });
+  });
+});
+
+app.get('/tambah-jenis', (req,res) => {
+  res.render('manager/tambah/tambah-jenis.ejs');
+});
+
+app.post('/add-jenis', (req,res)=>{
+  let data = req.body.nama_jenis;
+  let sql = 'INSERT INTO jenis SET ?';
+  let query = conn.query(sql,data,(err, results) => {
+    if(err) throw err;
+    res.redirect('/jenis');
+  });
+});
+
+app.post('/delete-jenis', (req,res)=>{
+  let sql = "DELETE FROM jenis WHERE no_jenis="+req.body.no_jenis+"";
+  let query = conn.query(sql,data,(err, results) => {
+    if(err) throw err;
+    res.redirect('/jenis');
+  });
+});
+
+
+// app.post('/add-karyawan',(req, res) => {
 //   let data = {
-//     nama_kasir: req.body.nama_kasir, 
-//     no_hp: req.body.no_hp,
-//     alamat: req.body.alamat_kasir,
-//     kode_kasir: req.body.kode_kasir
+//     no_karyawan: req.body.nama_kasir, 
+//     nama_karyawan: req.body.no_hp,
+//     nomor_handphone: req.body.alamat_kasir,
+//     jenis_kelamin: req.body.kode_kasir,
+//     tanggal_rekrut: req.body.kode_kasir,
+//     jabatan: req.body.kode_kasir,
+//     kode: req.body.kode_kasir,
+//     alamat: req.body.kode_kasir
 //   };
 //   let sql = "INSERT INTO kasir SET ?";
 //   let query = conn.query(sql, data,(err, results) => {
@@ -204,19 +304,29 @@ app.get('/jenis', (req,res) => {
 //   });
 // });
 
+
+
+// app.get('/edit-karyawan', (req,res) => {
+//   res.render('manager/edit/edit-karyawan.ejs');
+// });
+
+// app.get('/manager/kasir',(req,res)=>{
+  
+// });
+
+
+// app.post('/delete-karyawan',(req, res) => {
+//   let sql = "DELETE FROM karyawan WHERE id="+req.body.id+"";
+//   let query = conn.query(sql, (err, results) => {
+//     if(err) throw err;
+//       res.redirect('/manager/kasir');
+//   });
+// });
 // app.post('/update-kasir',(req, res) => {
 //   let sql = "UPDATE kasir SET nama_kasir='"+req.body.nama_kasir+"', no_hp='"+req.body.no_hp+"', alamat='"+req.body.alamat_kasir+"', kode_kasir='"+req.body.kode_kasir+"' WHERE id="+req.body.id;
 //   let query = conn.query(sql, (err, results) => {
 //     if(err) throw err;
 //     res.redirect('/manager/kasir');
-//   });
-// });
-
-// app.post('/delete-kasir',(req, res) => {
-//   let sql = "DELETE FROM kasir WHERE id="+req.body.id+"";
-//   let query = conn.query(sql, (err, results) => {
-//     if(err) throw err;
-//       res.redirect('/manager/kasir');
 //   });
 // });
 
